a4890e272793896db11f09a32d4a8925741c37bc
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-01 23:59:51 +0100

    samll fix

--- src/users/interceptors/current-user.interceptor.ts
+++ src/users/interceptors/current-user.interceptor.ts
@@ -12,7 +12,7 @@ export class CurrentUserInterceptor implements NestInterceptor {
   constructor(private usersService: UsersService) {}
   async intercept(context: ExecutionContext, handler: CallHandler<any>) {
     const request = context.switchToHttp().getRequest();
-    const { userId } = request.session;
+    const { userId } = request.session || {};
     if (userId) {
       const user = await this.usersService.findOne(userId);
       request.currentUser = user;
