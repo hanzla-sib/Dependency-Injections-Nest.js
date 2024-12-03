72bb218808960e8429e8b26711e8c14af9fe61b4
hanzla sibghat <hanzlasib24@gmail.com>
2024-12-03 09:53:12 +0100

    Dotenv module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Report } from 'src/reports/reports.entity';
 import { CurrentUserInterceptor } from 'src/users/interceptors/current-user.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthService } from 'src/users/auth.service';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
     ReportsModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get<string>('DB_NAME'),
+        entities: [User, Report],
+        synchronize: true,
+      }),
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
   ],
+
   controllers: [AppController],
   providers: [
     AppService,
